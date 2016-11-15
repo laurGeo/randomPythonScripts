87250e267f01b0e1a168c3820c0a2f2e8f6523ab
laurGeo <laureng5289@gmail.com>
2016-11-15 22:22:33 +0000

    Added variations

--- WordProceedingConsist.py
+++ WordProceedingConsist.py
@@ -1,15 +1,19 @@
 #This script takes a string. If the word consist is not followed by 'of', it replaces the next word with
 
-string = "It should consist in"
+string = "It consists in, consisting in, should consist as"
 ssplit = string.split()
 L = list()
 for word in ssplit:
 	L.append(word)
 
 for word in L:
-	if word == "consist":
+	if word == "consist" or word == "consists" or word == "consisting":
 		indexOfwordAfter = L.index(word) + 1;
 		if(L[indexOfwordAfter] != "of"):
 			L[indexOfwordAfter] = "of"
-			print "New sentence is now: "
-			print ' '.join(L)
+
+		newSentence = ' '.join(L)
+print "Old string: \n" + string
+print "New string: \n" + newSentence
+
+
